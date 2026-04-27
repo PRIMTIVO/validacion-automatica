a95145e57850852e7a04a6e6879abd86bda39449
PRIMTIVO <primitanque150@gmail.com>
2026-04-27 13:11:57 +0200

    Ejemplo de script.sh
    Añade script.sh con comandos básicos.

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hola Compañeros"
+
+# Ejemplo de código correcto
+nombre="GitHub"
+echo "Validando con $nombre Actions"
+
+# Ejemplo con error potencial (para probar)
+# variable=valor
+# echo $variable_sin_comillas
