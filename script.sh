4d27892075e0f056b41248fe011379fc0e647f21
PRIMTIVO <primitanque150@gmail.com>
2026-04-27 14:33:47 +0200

    Provocar el error en script.sh

--- script.sh
+++ script.sh
@@ -7,5 +7,14 @@ nombre="GitHub"
 echo "Validando con $nombre Actions"
 
 # Ejemplo con error potencial (para probar)
-# variable=valor
-# echo $variable_sin_comillas
+# ERROR 1: Variable sin comillas (shellcheck lo detecta)
+archivo="mi archivo.txt"
+rm $archivo
+
+# ERROR 2: Comparación incorrecta
+if [ $variable = "valor" ]
+then
+    echo "Igual"
+fi
+
+echo "Hola Mundo"
